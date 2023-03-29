99e9e3c72324c211e96e981fe52b76160cbafd0d
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-29 22:26:47 +0600

    Add files via upload

---
+++ 29_Mar_3_calculator_nafiz_sir_division.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class Calculator{
+    int val1;
+    int val2;
+    public:
+    void set_val_1(int p){
+        val1=p;
+    }
+    void set_val_2(int q){
+        val2=q;
+    }
+    int get_val_1(){
+        return val1;
+    }
+     int get_val_2(){
+        return val2;
+    }
+    double division(){
+        return val1/val2;
+    }
+};
+int main(){
+    Calculator c1;
+    c1.set_val_1(10);
+    c1.set_val_2(10);
+    cout<<c1.get_val_1()<<endl;
+    cout<<c1.get_val_2()<<endl;
+    cout<<"Division: "<<c1.division()<<endl;
+}
+
+
