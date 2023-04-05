bf87a56b8d808e8ff708989a03d1f730b2fcfdf9
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-04-05 15:57:41 +0600

    Add files via upload

---
+++ 5_apr_OOP_practice_04.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class stu{
+public:
+    void print();
+};
+int main(){
+    stu a;
+    a.print();
+    return 0;
+}
+void stu :: print(){
+         cout<<101<<endl<<"abdur"<<endl<<3.987;
+    }
