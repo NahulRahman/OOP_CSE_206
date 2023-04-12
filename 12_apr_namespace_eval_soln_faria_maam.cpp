48245274b5a6a5aff0533b0d5594ece4e68afa04
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-04-12 18:03:55 +0600

    Add files via upload
    this is an incomplete code. use it with precautions

---
+++ 12_apr_namespace_eval_soln_faria_maam.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+namespace performance{
+    class employee{
+        int id;
+        int pref;
+    public:
+        void setperf(){
+            cin>>perf;
+        }
+        int getperf(){
+            return perf;
+        }
+    };
+}
+namespace salary{
+    class employee{
+        float sal;
+    public:
+
+    };
+}
