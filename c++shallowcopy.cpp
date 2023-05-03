7487dd4232d49c0f03cb9c31ef0078df42fbf2ea
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-05-03 16:09:46 +0600

    Add files via upload

---
+++ c++shallowcopy.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io()                     \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+
+#define int long long // define int as a long long data type
+#define endl '\n'
+class myclass{
+    public:
+    int *arr ;
+    int size;
+    public:
+    myclass(){};
+    myclass(const myclass & obj){
+        arr=obj.arr;
+        size=obj.size;
+
+    }
+};
+
+signed main()
+{
+    fast_io(); // enable faster I/O
+    myclass obj1;
+    obj1.size=5;
+    obj1.arr=new int [obj1.size];
+    for(int i=0;i<obj1.size;i++){
+        obj1.arr[i]=i;
+    }
+    myclass obj2=obj1;
+    obj1.arr[0]=100;
+    cout<<obj2.arr[0]<<endl;
+
+    return 0;
+}
