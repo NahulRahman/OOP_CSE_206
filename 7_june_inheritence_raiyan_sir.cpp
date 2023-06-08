f26e5daa0e806005b4861062910ae92f8a68c681
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-06-09 01:42:12 +0600

    Add files via upload

---
+++ 7_june_inheritence_raiyan_sir.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using anmespace std;
+
+class Person{
+    double contact;
+public:
+    string name;
+    double id;
+protected:
+
+};
+class Employee:public Person{
+    double BasicSal;
+public:
+    double bonus;
+    Employee(){
+        int getsal(){
+            return
+        }
+    }
+};
+class Operator: public Employee{
+
+};
+
+int main(){
+    Person P;
+
+    return 0;
+}
