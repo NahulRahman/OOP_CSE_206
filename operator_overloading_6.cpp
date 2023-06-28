8fd8b38b7dfd7534f44804d2dd757582c4ec2b15
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-06-28 21:01:38 +0600

    Add files via upload

---
+++ operator_overloading_6.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+    char *name;
+    int len;
+    int roll;
+
+public:
+    Student(char *name, int len, int roll)
+    {
+        this->len = len;
+        this->name = new char[len + 1];
+        for (int i = 0; i < len; i++)
+            this->name[i] = name[i];
+        this->name[len] = '\0';
+        this->roll = roll;
+    }
+    void print()
+    {
+
+        cout << name << " " << len << " " << roll << endl;
+    }
+    void operator++(int)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            this->name[i]++;
+        }
+    }
+    ~Student() { delete[] name; }
+};
+int main()
+{
+    Student s1("CPP", 3, 202114000);
+    s1++;
+    s1.print();
+}
